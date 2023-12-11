c552e5a4f2e28881b84295aff89f09f35e3db6e5
승훈 정 <vcd0h24010@gmail.com>
2023-12-11 17:44:24 +0900

    23.12.11업데이트

---
+++ PROJECT_TT/Source/PROJECT_TT/PROJECT_TT.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PROJECT_TT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PROJECT_TT, "PROJECT_TT" );
---
+++ PROJECT_TT/Source/PROJECT_TT/PROJECT_TTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PROJECT_TTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TT_API APROJECT_TTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
